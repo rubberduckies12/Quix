538ba740b3132589c427a233d04b7dbed0ceb1d4
rubberduckies12 <rowetommy699@gmail.com>
2025-10-24 21:21:15 +0100

    fixed header and footer in privacy policy

--- web/src/pages/policy/privacy/privacy.tsx
+++ web/src/pages/policy/privacy/privacy.tsx
@@ -1,7 +1,7 @@
 'use client';
 import React from 'react';
-import Header from '../../components/header';
-import Footer from '../../components/footer';
+import Header from '../../../components/header';
+import Footer from '../../../components/footer';
 
 const Cookies = () => {
   return (
